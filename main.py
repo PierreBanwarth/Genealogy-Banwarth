84a4700e110ede80c2aceb6f7c6447840fb4cc3c
Banwarth Pierre <pi.banwarth@gmail.com>
2021-12-26 19:32:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,5 @@ from meza import io
 
 Enregistrements = io.read('table1.mdb') # only file path, no file objects
 
-test = next(Enregistrements)
-while test:
-    print(test)
+for x in Enregistrements
+    print(x)
