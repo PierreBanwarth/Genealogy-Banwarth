a2c1786de76124b95ed46c61bfc356890967e5de
Banwarth Pierre <pi.banwarth@gmail.com>
2021-12-26 19:39:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ from meza import io
 Enregistrements = io.read('table1.mdb') # only file path, no file objects
 
 for x in Enregistrements:
-    print(x.decode("utf-16"))
+    with open(x, encoding="utf-16") as f:
+        print(x)
